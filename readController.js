e6aa62a20a1c0455dd73570993ef54eb65e3af5c
Mehul Chaturvedi <mechat0141@gmail.com>
2019-05-14 06:01:48 +0530

    Update readController.js

--- readController.js
+++ readController.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const csv=require('csvtojson')
 
-
+//function splites the string into array 
 var readFile = (title)=>{
+    //splites string into array and stores in different index when it encounters " "
     title = title.split(" ");
     title.forEach((ele)=>{
         console.log(ele, 'e')
@@ -11,6 +12,7 @@ var readFile = (title)=>{
     
 }
 
+//csvtojson to print response in a good format
 function readELE(title){
     var noteString = fs.readFileSync('./files/'+title, { encoding: 'utf8' });
     csv({
@@ -19,6 +21,7 @@ function readELE(title){
     })
     .fromString(noteString)
     .subscribe((csvRow)=>{ 
+        //to print only those includes LTC-AUD, ZEC-AUD and BTC-AUD
         if(csvRow.includes('LTC-AUD') || csvRow.includes('ZEC-AUD') || csvRow.includes('BTC-AUD')){
         var csvRow = csvRow.replace(/,/g, '\t');
         console.log(csvRow)
